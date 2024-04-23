77068cca0205ec140cad801a29beba9ad33547d3
Daniel Chen <fadengchen1@gmail.com>
2024-04-23 20:40:46 +1000

    Remove cursor component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import Test from "./Test";
 import "./app.scss";
 import Contact from "./components/contact/Contact";
-import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
@@ -11,7 +9,6 @@ import Services from "./components/services/Services";
 const App = () => {
   return (
     <div>
-      <Cursor />
       <section id="Homepage">
         <Navbar />
         <Hero />
